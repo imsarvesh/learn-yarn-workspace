66b215930bb68768e590cbd9c05d362a5faf5639
SArvesh Kesharwani <me@imsarvesh.com>
2023-02-20 21:55:36 +0000

    initial commit

---
+++ packages/my-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Calendar from "@my-app/react-calendar";
+import '@my-app/react-calendar/dist/Calendar.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
